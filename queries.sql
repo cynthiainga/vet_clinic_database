83cdaa1c723cead08f85b88716a20e7e966fe609
Cynthia Inga <candyinga67@gmail.com>
2022-04-27 08:56:57 +0200

    Add command to find all animals not named Gabumon

--- queries.sql
+++ queries.sql
@@ -5,3 +5,4 @@ SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-0
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals WHERE neutered = true;
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
