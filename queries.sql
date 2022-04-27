09f8a3fca28609c37ab64a79bb0efa6ef8772d02
Cynthia Inga <candyinga67@gmail.com>
2022-04-27 08:52:22 +0200

    Add command to select date_of_birth of animals named either 'Agumon' or 'Pikachu'

--- queries.sql
+++ queries.sql
@@ -3,3 +3,4 @@
 SELECT * FROM animals WHERE name LIKE '%mon';
 SELECT name FROM animals WHERE date_of_birth BETWEEN '2016-01-01' AND '2019-01-01';
 SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
