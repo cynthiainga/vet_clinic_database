47e7cbeb12ad32a6438fc704dd5095d0168ceba4
Cynthia Inga <candyinga67@gmail.com>
2022-04-27 09:06:48 +0200

    Add command to find all animals with weights that equals precisely 10.4kg or 17.3kg

--- queries.sql
+++ queries.sql
@@ -6,3 +6,4 @@ SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
+SELECT name FROM animals WHERE weight_kg = 10.4 OR weight_kg = 17.3;
