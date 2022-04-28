dad3cf5f14b903ace950c1665e8b75a9d8554f24
Cynthia Inga <candyinga67@gmail.com>
2022-04-28 16:18:19 +0200

    Make some changes

--- queries.sql
+++ queries.sql
@@ -1,8 +1,8 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth BETWEEN '2016/01/01' AND '2019/12/31';
-SELECT name FROM animals WHERE neutered = true AND escape_attempts < 3;
+SELECT name FROM animals WHERE date_of_birth  BETWEEN '2016/01/01' AND '2019/12/31';
+SELECT name FROM animals WHERE neutered = true  AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals WHERE neutered = true;
 SELECT * FROM animals WHERE NOT name = 'Gabumon';
@@ -19,7 +19,7 @@ DELETE FROM animals;
 ROLLBACK;
 
 BEGIN;
-DELETE FROM animals WHERE data_of_birth > '2022/01/01';
+DELETE FROM animals WHERE date_of_birth > '2022/01/01';
 SAVEPOINT SP1;
 UPDATE animals SET weight_kg = -1 * weight_kg;
 ROLLBACK TO SP1;
@@ -31,4 +31,4 @@ SELECT COUNT(*) FROM animals WHERE escape_attempts = 0;
 SELECT AVG(weight_kg) FROM animals;
 SELECT id, name, escape_attempts FROM animals WHERE escape_attempts = (SELECT MAX(escape_attempts) FROM animals) AND (neutered = true OR neutered = false);
 SELECT MIN(weight_kg), MAX(weight_kg) FROM animals;
-SELECT AVG(escape_attempts) FROM animals WHERE data_of_birth BETWEEN '1990/01/01' AND '2000/12/31';
+SELECT AVG(escape_attempts) FROM animals WHERE date_of_birth  BETWEEN '1990/01/01' AND '2000/12/31';
