eb995e026da05c0a14159b263ed8da69d54a6bf1
Cynthia Inga <candyinga67@gmail.com>
2022-04-28 17:09:14 +0200

    make changes

--- queries.sql
+++ queries.sql
@@ -1,13 +1,17 @@
 /*Queries that provide answers to the questions from all projects.*/
 
 SELECT * FROM animals WHERE name LIKE '%mon';
-SELECT name FROM animals WHERE date_of_birth  BETWEEN '2016/01/01' AND '2019/12/31';
+SELECT * FROM animals WHERE date_of_birth  BETWEEN '2016/01/01' AND '2019/12/31';
 SELECT name FROM animals WHERE neutered = true  AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE NOT name = 'Gabumon';
-SELECT name FROM animals WHERE weight_kg > 10.5;
+SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
+
+BEGIN;
+UPDATE animals set species = 'unspecified' WHERE species IS NULL;
+ROLLBACK; 
 
 BEGIN;
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
@@ -15,15 +19,15 @@ UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 
 BEGIN;
-DELETE FROM animals;
+DELETE * FROM animals;
 ROLLBACK;
 
 BEGIN;
 DELETE FROM animals WHERE date_of_birth > '2022/01/01';
-SAVEPOINT SP1;
+SAVEPOINT sp1;
 UPDATE animals SET weight_kg = -1 * weight_kg;
-ROLLBACK TO SP1;
-UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
+ROLLBACK TO sp1;
+UPDATE animals SET weight_kg = (SELECT weight_kg FROM animals WHERE weight_kg < 0)*-1;
 COMMIT;
 
 SELECT COUNT(*) FROM animals;
