c54fe62b71130fcd1f6e5ee6f8d096d61ba68c8c
Cynthia Inga <candyinga67@gmail.com>
2022-04-28 18:37:46 +0200

    Fix typo error

--- queries.sql
+++ queries.sql
@@ -5,12 +5,12 @@ SELECT * FROM animals WHERE date_of_birth  BETWEEN '2016/01/01' AND '2019/12/31'
 SELECT name FROM animals WHERE neutered = true  AND escape_attempts < 3;
 SELECT date_of_birth FROM animals WHERE name = 'Agumon' OR name = 'Pikachu';
 SELECT * FROM animals WHERE neutered = true;
-SELECT * FROM animals WHERE name <> 'Gabumon';
+SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT name, escape_attempts FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
-UPDATE animals set species = 'unspecified' WHERE species IS NULL;
+UPDATE animals SET species = 'unspecified' WHERE species IS NULL;
 ROLLBACK; 
 
 BEGIN;
@@ -19,7 +19,7 @@ UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
 
 BEGIN;
-DELETE * FROM animals;
+DELETE FROM animals;
 ROLLBACK;
 
 BEGIN;
@@ -27,7 +27,7 @@ DELETE FROM animals WHERE date_of_birth > '2022/01/01';
 SAVEPOINT sp1;
 UPDATE animals SET weight_kg = -1 * weight_kg;
 ROLLBACK TO sp1;
-UPDATE animals SET weight_kg = (SELECT weight_kg FROM animals WHERE weight_kg < 0)*-1;
+UPDATE animals SET weight_kg = -1 * weight_kg WHERE weight_kg < 0;
 COMMIT;
 
 SELECT COUNT(*) FROM animals;
