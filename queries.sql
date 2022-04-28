659cbaa31088961cd7905df061314a05b1de3f2c
Cynthia Inga <candyinga67@gmail.com>
2022-04-28 05:08:14 +0200

    Add the command to start the Transaction

--- queries.sql
+++ queries.sql
@@ -9,5 +9,7 @@ SELECT * FROM animals WHERE NOT name = 'Gabumon';
 SELECT name FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
+BEGIN;
+
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
