6e7b544ff821d4c2a64966e6836ea5674025697f
Cynthia Inga <candyinga67@gmail.com>
2022-04-28 05:13:52 +0200

    I Delete all records in animals table and then I Rollback the transaction

--- queries.sql
+++ queries.sql
@@ -10,7 +10,10 @@ SELECT name FROM animals WHERE weight_kg > 10.5;
 SELECT * FROM animals WHERE weight_kg >= 10.4 AND weight_kg <= 17.3;
 
 BEGIN;
-
 UPDATE animals SET species = 'digimon' WHERE name LIKE '%mon';
 UPDATE animals SET species = 'pokemon' WHERE species IS NULL;
 COMMIT;
+
+BEGIN;
+DELETE FROM animals;
+ROLLBACK;
