8cb3054fe47c190f8be09565f5eccde94e41bb9b
Cynthia Inga <candyinga67@gmail.com>
2022-04-29 10:25:29 +0200

    Insert data into specialization table

--- data.sql
+++ data.sql
@@ -56,3 +56,9 @@ VALUES ('William Tatcher', 45, '2000/04/23'),
        ('Maisy Smith', 26, '2019/01/17'),
        ('Stephanie Mendez', 64, '1981/05/04'),
        ('Jack Harkness', 38, '2008/06/08');
+
+INSERT INTO specializations(vets_id, species_id)
+VALUES (1,1),
+       (1,3),
+       (2,3),
+       (2,4);
